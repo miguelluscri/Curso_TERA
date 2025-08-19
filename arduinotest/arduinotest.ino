d227af7d5597139c61d353a970bdcc0336e63aa8
miguelluscri <miguel.luscri@unesp.br>
2025-08-19 16:40:45 -0300

    pinto

---
+++ arduinotest/arduinotest.ino
@@ -0,0 +1,18 @@
+const int ledPin = 13;
+
+void setup() {
+
+  pinMode(ledPin, OUTPUT);
+
+}
+
+VOID LOOP() {
+
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+
+}
+
+digitalWrite(ledPin, LOW);
+delay(1000);
+}
